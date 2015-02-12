87510e7a8238a8673c55d42818882425c8370b3d
Thomas A. McGonagle <mcgonagle_thomas@bah.com>
2015-02-12 12:07:47 -0600

    Changed localhost parameters in default[jellyfish][api][cors_allow_origin] to localhost:*

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,9 +6,9 @@ default["jellyfish"]["ux"]["enabled"] = "true"
 
 # The CORS for Jellyfish-API
 # Comma seperated list of host that you want to be able to access Jellyfish API
-default["jellyfish"]["api"]["cors_allow_origin"] = "127.0.0.1:5000,localhost:5000,localhost:80,localhost:443"
+default["jellyfish"]["api"]["cors_allow_origin"] = "localhost:*"
 
 # The URL of the Jellyfish-API that UX should be using
 # Single FQDN: https://some-host.company.com:port (if port is different that the standard port)
-default["jellyfish"]["ux"]["app_config_js"] = "localhost:3000"
+default["jellyfish"]["ux"]["app_config_js"] = "localhost:3030"
 
