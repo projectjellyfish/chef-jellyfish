d7370067a3e9e386fdc0b979af8c91ceed0d64e3
Thomas A. McGonagle <mcgonagle_thomas@bah.com>
2015-02-24 13:59:05 -0500

    replaced all double quotes with single quotes to pass travis rubocop

--- recipes/ux.rb
+++ recipes/ux.rb
@@ -35,64 +35,64 @@ bash 'Install Node' do
   creates '/usr/bin/gulp'
 end
 
-log "Checkout and Unzip the latest UX Code"
-remote_file "/home/jellyfish/ux-master.zip" do
-  source "https://github.com/projectjellyfish/ux/archive/master.zip"
+log 'Checkout and Unzip the latest UX Code'
+remote_file '/home/jellyfish/ux-master.zip' do
+  source 'https://github.com/projectjellyfish/ux/archive/master.zip'
   mode '0644'
 end
 
-log "Unzip ux-master and move it to ux"
+log 'Unzip ux-master and move it to ux'
 bash 'unzip ux-master.zip' do
-  cwd "/home/jellyfish"
+  cwd '/home/jellyfish'
   user "jellyfish"
   code <<-EOH
   unzip ux-master.zip
   EOH
-  creates "/home/jellyfish/ux-master"
+  creates '/home/jellyfish/ux-master'
 end
 
-bash "mv ux-master ux" do
-  cwd "/home/jellyfish"
-  user "jellyfish"
+bash 'mv ux-master ux' do
+  cwd '/home/jellyfish'
+  user 'jellyfish'
   code <<-EOH
    mv /home/jellyfish/ux-master /home/jellyfish/ux
   EOH
-  creates "/home/jellyfish/ux"
+  creates '/home/jellyfish/ux'
 end
 
-log "Run gulp and Install into Production"
-bash "Install npm, forever and gulp production" do
-  user "root"
-  cwd "/home/jellyfish/ux"
+log 'Run gulp and Install into Production'
+bash 'Install npm, forever and gulp production' do
+  user 'root'
+  cwd '/home/jellyfish/ux'
   code <<-EOH
   /usr/bin/npm link
   /usr/bin/npm install -g 
   /usr/bin/gulp production
   /usr/bin/npm install forever  -g 
   EOH
-  creates "/home/jellyfish/ux/node_modules/winston"
+  creates '/home/jellyfish/ux/node_modules/winston'
 end
 
 log 'Set ENV settings'
 template '/opt/ux-master/public/appConfig.js' do
   source 'appConfig.js.erb'
-  mode '0644'
-  owner 'jellyfish'
-  group 'jellyfish'
-end
-
-log "Change user permissions appVersion.js"
-file "/home/jellyfish/ux/public/appVersion.js" do
   owner 'jellyfish'
   group 'jellyfish'
   mode '0644'
 end
 
+log 'Change user permissions appVersion.js'
+file '/home/jellyfish/ux/public/appVersion.js' do
+  owner 'jellyfish'
+  group 'jellyfish'
+  mode '0644'
+end
 
-log "Run node"
-bash "run node" do
-  user "jellyfish"
-  cwd "/home/jellyfish/ux"
+
+log 'Run node'
+bash 'run node' do
+  user 'jellyfish'
+  cwd '/home/jellyfish/ux'
   code <<-EOH
   /usr/bin/forever start app.js &
   touch /tmp/node_is_running
