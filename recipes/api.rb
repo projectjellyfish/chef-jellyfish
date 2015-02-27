96d2cf1a6c42d7e3126324e7af584f2afafcbca4
Thomas A. McGonagle <mcgonagle_thomas@bah.com>
2015-02-27 13:42:59 -0500

    api.rb recipe with attribute variables

--- recipes/api.rb
+++ recipes/api.rb
@@ -6,6 +6,10 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+node.set['rbenv']['root']          = rbenv_root_path
+node.set['ruby_build']['prefix']   = "#{node['rbenv']['root']}/plugins/ruby_build"
+node.set['ruby_build']['bin_path'] = "#{node['ruby_build']['prefix']}/bin"
+
 log 'Create jellyfish user'
 user 'jellyfish' do
   comment 'jellyfish user'
@@ -32,21 +36,91 @@ yum_package 'bison'
 yum_package 'sqlite-devel'
 yum_package 'unzip'
 
-log 'Install rbenv/rbenv-build/rbenv-vars'
-include_recipe 'rbenv::default'
-include_recipe 'rbenv::ruby_build'
-include_recipe 'rbenv::rbenv_vars'
-
-log 'Installing rbenv ruby 2.1.5'
-rbenv_ruby '2.1.5'
-
-rbenv_gem 'rails' do
-  ruby_version '2.1.5'
+log 'Checkout the latest code'
+remote_file '/home/jellyfish/api-master.zip' do
+  source 'https://github.com/projectjellyfish/api/archive/master.zip'
+  mode '0644'
 end
 
-# rbenv_gem 'passenger' do
-#   ruby_version '2.1.5'
-# end
+bash 'unzip api-master.zip' do
+  cwd '/home/jellyfish'
+  user 'jellyfish'
+  code <<-EOH
+  unzip api-master.zip
+  EOH
+  creates '/home/jellyfish/api-master'
+end
+
+bash 'mv api-master api' do
+  cwd '/home/jellyfish'
+  user 'jellyfish'
+  code <<-EOH
+   mv /home/jellyfish/api-master /home/jellyfish/api
+  EOH
+  creates '/home/jellyfish/api'
+end
+
+directory "node['rbenv']['root_path']" do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+#git "node['rbenv']['root_path']" do
+#  repository "https://github.com/sstephenson/rbenv.git" 
+#  revision "master"
+#  action :sync
+#  user node['rbenv']['user']
+#  group node['rbenv']['group']
+#end
+
+
+
+template "/home/jellyfish/.bash_profile" do
+  source "bash_profile.erb"
+  mode "0644"
+  notifies :create, "ruby_block[initialize_rbenv]", :immediately
+end
+
+ruby_block "initialize_rbenv" do
+  block do
+    ENV['RBENV_ROOT'] = node['rbenv']['root']
+    ENV['PATH'] = "#{node['rbenv']['root']}/bin:#{node['rbenv']['root']}/shims:#{node['ruby_build']['bin_path']}:#{ENV['PATH']}"
+  end
+
+  action :nothing
+end
+
+# rbenv init creates these directories as root because it is called
+# from /etc/profile.d/rbenv.sh But we want them to be owned by rbenv
+# check https://github.com/sstephenson/rbenv/blob/master/libexec/rbenv-init#L71
+%w{shims versions plugins}.each do |dir_name|
+  directory "#{node['rbenv']['root']}/#{dir_name}" do
+    owner node['rbenv']['user']
+    group node['rbenv']['group']
+    mode "2775"
+    action [:create]
+  end
+end
+
+git node['ruby_build']['prefix'] do
+  repository node['ruby_build']['git_repository']
+  reference node['ruby_build']['git_revision']
+  action :sync
+  user node['rbenv']['user']
+  group node['rbenv']['group']
+end
+
+log 'Installing Ruby 2.2.0'
+bash 'install ruby 2.2.0' do
+  cwd '/home/jellyfish'
+  user 'jellyfish'
+  code <<-EOH
+   source /home/jellyfish/.bash_profile && /home/jellyfish/.rbenv/bin/rbenv install 2.2.0
+  EOH
+  creates '/home/jellyfish/.rbenv/versions/2.2.0'
+end
 
 log 'Install PostgreSQL'
 remote_file '/opt/pgdg-redhat93-9.3-1.noarch.rpm' do
@@ -64,27 +138,29 @@ yum_package 'postgresql93-server'
 yum_package 'postgresql93-devel'
 yum_package 'postgresql93-contrib'
 
+log 'Install gem pg'
 bash 'gem instal pg' do
-  cwd '/opt/'
-  user 'root'
+  cwd '/home/jellyfish/'
+  user 'jellyfish'
   code <<-EOH
-  source /etc/profile.d/rbenv.sh
-  rbenv global 2.1.5
-  /opt/rbenv/versions/2.1.5/bin/gem install pg -v '0.17.1' -- \
-  --with-pg-config=/usr/pgsql-9.3/bin/pg_config
+   source /home/jellyfish/.bash_profile
+   /home/jellyfish/.rbenv/bin/rbenv global 2.2.0
+   /home/jellyfish/.rbenv/versions/2.2.0/bin/gem install pg -v '0.17.1' -- \
+   --with-pg-config=/usr/pgsql-9.3/bin/pg_config
   EOH
-  creates '/opt/rbenv/versions/2.1.5/lib/ruby/gems/2.1.0/gems/pg-0.18.1'
+  creates '/home/jellyfish/.rbenv/versions/2.2.0/lib/ruby/gems/2.2.0/gems/pg-0.17.1'
 end
 
+log 'Install gem sqlite3'
 bash 'gem instal sqlite3' do
-  cwd '/opt/'
-  user 'root'
+  cwd '/home/jellyfish/'
+  user 'jellyfish'
   code <<-EOH
-  source /etc/profile.d/rbenv.sh
-  rbenv global 2.1.5
-  /opt/rbenv/versions/2.1.5/bin/gem install sqlite3 -v '1.3.10'
+  source /home/jellyfish/.bash_profile
+  /home/jellyfish/.rbenv/bin/rbenv global 2.2.0
+  /home/jellyfish/.rbenv/versions/2.2.0/bin/gem install sqlite3 -v '1.3.10'
   EOH
-  creates '/opt/rbenv/versions/2.1.5/lib/ruby/gems/2.1.0/gems/sqlite3-1.3.10'
+  creates '/home/jellyfish/.rbenv/versions/2.2.0/lib/ruby/gems/2.2.0/gems/sqlite3-1.3.10'
 end
 
 bash 'sed requiretty sudoers' do
@@ -96,47 +172,40 @@ bash 'sed requiretty sudoers' do
   not_if('grep requiretty /etc/sudoers|grep ^#Defaults')
 end
 
-log 'Checkout the latest code'
-remote_file '/opt/api-master.zip' do
-  source 'https://github.com/projectjellyfish/api/archive/master.zip'
-  mode '0644'
-end
-
-bash 'unzip api-master.zip' do
-  cwd '/opt'
-  user 'root'
-  code <<-EOH
-  unzip api-master.zip
-  EOH
-  creates '/opt/api-master'
-end
 
 log 'Application.yml configuration file'
-template '/opt/api-master/.env' do
+template '/home/jellyfish/api/.env' do
   source 'dotEnv.erb'
   mode '0644'
-  owner 'root'
-  group 'root'
+  owner 'jellyfish'
+  group 'jellyfish'
 end
 
 log 'Install bundler'
-rbenv_gem 'bundler' do
-  ruby_version '2.1.5'
-end
-
-log 'bundle api-master'
-bash 'bundle api-master' do
-  cwd '/opt/api-master'
+bash 'gem instal bundler' do
+  cwd '/home/jellyfish/.rbenv/'
   user 'root'
   code <<-EOH
-  source /etc/profile.d/rbenv.sh && bundle
+  source /home/jellyfish/.bash_profile
+  /home/jellyfish/.rbenv/bin/rbenv global 2.2.0
+  /home/jellyfish/.rbenv//versions/2.2.0/bin/gem install bundler
   EOH
-  creates '/opt/rbenv/versions/2.1.5/lib/ruby/gems/2.1.0/gems/xml-simple-1.1.4'
+  creates '/home/jellyfish/.rbenv/versions/2.2.0/lib/ruby/gems/2.2.0/gems/bundler'
+end
+
+log 'bundle api'
+bash 'bundle api' do
+  cwd '/home/jellyfish/api'
+  user 'jellyfish'
+  code <<-EOH
+  source /home/jellyfish/.bash_profile && bundle
+  EOH
+  creates '/home/jellyfish/.rbenv/versions/2.2.0/lib/ruby/gems/2.2.0/gems/xml-simple-1.1.4'
 end
 
 log 'Populate the database'
 bash 'postgresql 9.3 initdb ' do
-  cwd '/opt/api-master'
+  cwd '/home/jellyfish/api'
   user 'root'
   code <<-EOH
   service postgresql-9.3 initdb
@@ -144,20 +213,12 @@ bash 'postgresql 9.3 initdb ' do
   creates '/var/lib/pgsql/9.3/data/base/1'
 end
 
+log 'Convfiguring postgres database'
 service 'postgresql-9.3' do
   action :start
 end
 
-bash 'assign-postgres-password' do
-  user 'postgres'
-  code <<-EOH
-  echo 'ALTER ROLE postgres PASSWORD 'password';' | psql
-  touch /tmp/alter_pgsql
-  EOH
-  action :run
-  creates '/tmp/alter_pgsql'
-end
-
+log 'Template pg_hba.conf'
 template '/var/lib/pgsql/9.3/data/pg_hba.conf' do
   source 'pg_hba.conf.erb'
   mode '0600'
@@ -169,25 +230,26 @@ service 'postgresql-9.3 restart' do
   action :restart
 end
 
-log 'running rake tasks'
+log 'Running rake tasks'
 bash 'rake tasks' do
-  cwd '/opt/api-master'
-  user 'root'
+  cwd '/home/jellyfish/api'
+  user 'jellyfish'
   code <<-EOH
-  source /etc/profile.d/rbenv.sh
+  source /home/jellyfish/.bash_profile.sh
   rake db:drop db:create db:migrate
   rake db:seed && touch /tmp/rake_db_create
   EOH
   creates '/tmp/rake_db_create'
 end
 
-log 'starting rails'
-bash 'rails s' do
-  cwd '/opt/api-master'
-  user 'root'
+log 'Starting rails'
+bash 'rails s -d' do
+  cwd '/home/jellyfish/api'
+  user 'jellyfish'
   code <<-EOH
-  source /etc/profile.d/rbenv.sh && /opt/api-master/bin/rails s -d &
+  source /home/jellyfish/.bash_profile && /home/jellyfish/api/bin/rails s -d &
   touch /tmp/rails_started
   EOH
   creates '/tmp/rails_started'
 end
+
