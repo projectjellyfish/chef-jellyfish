51761b89981d536198187b19a66f30dcd6d5d30b
Chris Kacerguis <kacerguis_christopher@bah.com>
2015-05-28 12:49:15 -0500

    Gemfile

---
+++ test/support/Gemfile.rb
@@ -0,0 +1,27 @@
+ruby '1.9.3'
+
+source 'https://rubygems.org'
+
+gem 'berkshelf'
+
+# Uncomment these lines if you want to live on the Edge:
+#
+# group :development do
+#   gem "berkshelf", github: "berkshelf/berkshelf"
+#   gem "vagrant", github: "mitchellh/vagrant", tag: "v1.6.3"
+# end
+#
+# group :plugins do
+#   gem "vagrant-berkshelf", github: "berkshelf/vagrant-berkshelf"
+#   gem "vagrant-omnibus", github: "schisamo/vagrant-omnibus"
+# end
+
+gem 'test-kitchen'
+gem 'kitchen-vagrant'
+
+# For easy publishing to Supermarket
+gem 'stove'
+
+gem 'rake'
+gem 'foodcritic'
+gem 'rubocop'
