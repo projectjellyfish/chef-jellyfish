f536ce62bc1c1baccd274a92d68ba12d3f0ed20e
Kevin Kingsbury <kkingsbury@gmail.com>
2015-06-25 10:16:32 -0500

    New Release Taging

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kingsbury_kevin@bah.com'
 license 'MIT'
 description 'Installs/Configures Jellyfish via a Chef Cookbook'
 long_description 'Installs/Configures Jellyfish via a Chef Cookbook'
-version '1.1.0'
+version '1.2.0'
 
 supports 'centos', '~> 6.0'
 supports 'rhel', '~> 6.0'
